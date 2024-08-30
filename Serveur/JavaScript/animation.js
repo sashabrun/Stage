a967ade6d73ebe7f6a781a27c1117e905daeefdd
Sasha <sasha.brun@ynov.com>
2024-08-30 17:12:14 +0200

    Refont

---
+++ Serveur/JavaScript/animation.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    // Attendre la fin de l'animation du logo (3 secondes)
+    setTimeout(function() {
+        // Rediriger vers la page d'accueil
+        window.location.href = "home.html"; // Remplacez "home.html" par le nom de la page cible
+    }, 3000);
+};
